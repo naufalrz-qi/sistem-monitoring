8602e29d8d785318d3c5f2f5bb29fabfb0cadcd6
Ari Efendi <ariefendi992@gmail.com>
2023-01-14 22:37:36 +0800

    fix, wali kelas rekap absen kelas

--- app/site/controller/wali_kelas_controller.py
+++ app/site/controller/wali_kelas_controller.py
@@ -1,3 +1,5 @@
+from calendar import monthrange
+from datetime import datetime
 from flask import (
     Flask,
     make_response,
@@ -11,7 +13,12 @@ from flask import (
 from flask_login import current_user, login_required
 from sqlalchemy import func
 from app.models.data_model import AbsensiModel
-from app.models.master_model import NamaBulanModel, WaliKelasModel
+from app.models.master_model import (
+    KepsekModel,
+    MengajarModel,
+    NamaBulanModel,
+    WaliKelasModel,
+)
 from app.models.user_details_model import SiswaModel
 from app.site.forms.form_letter_report import FormRekapAbsenWali
 from ...extensions import db
@@ -61,18 +68,67 @@ def data_siswa():
     return response
 
 
-@wali_kelas.route("rekap-absen")
+@wali_kelas.route("rekap-absen", methods=["GET", "POST"])
 @login_required
 def rekap_absen():
+    data = {}
+    data["kelas"] = sql_wali_().kelas.kelas
+    data["wali_kelas"] = f"{sql_wali_().guru.first_name} {sql_wali_().guru.last_name}"
+    data["nip_wali"] = sql_wali_().guru.user.username
+
+    sql_kepsek = KepsekModel.query.filter_by(status=1).first()
+    data["kepsek"] = f"{sql_kepsek.guru.first_name} {sql_kepsek.guru.last_name}"
+    data["nip_kepsek"] = f"{sql_kepsek.guru.user.username}"
     form = FormRekapAbsenWali()
-    sql_bulan = NamaBulanModel.query.all()
+    sql_bulan = NamaBulanModel.query
     sql_tahun = AbsensiModel.query.group_by(func.year(AbsensiModel.tgl_absen)).all()
 
-    for i in sql_bulan:
+    for i in sql_bulan.all():
         form.bulan.choices.append((i.id, i.nama_bulan.upper()))
 
     for i in sql_tahun:
         form.tahun.choices.append((i.tgl_absen.year, i.tgl_absen.year))
+
+    if form.validate_on_submit() and request.method == "POST":
+        bulan_id = request.form.get("bulan")
+        tahun = request.form.get("tahun")
+
+        sql_siswa = (
+            db.session.query(AbsensiModel)
+            .filter(AbsensiModel.mengajar_id == MengajarModel.id)
+            .filter(AbsensiModel.siswa_id == SiswaModel.user_id)
+            .filter(SiswaModel.kelas_id == sql_wali_().kelas_id)
+            .filter(func.month(AbsensiModel.tgl_absen) == bulan_id)
+            .filter(func.year(AbsensiModel.tgl_absen) == tahun)
+            .group_by(AbsensiModel.siswa_id)
+            .all()
+        )
+
+        sql_ket = db.session.query(AbsensiModel)
+
+        month_range = monthrange(int(tahun), int(bulan_id))
+        data["month_range"] = month_range[1]
+        data["bulan"] = sql_bulan.filter_by(id=bulan_id).first().nama_bulan
+        data["today"] = datetime.date(datetime.today())
+        data["semester"] = min([i.mengajar.semester.semester for i in sql_siswa])
+        data["ta"] = min([i.mengajar.tahun_ajaran.th_ajaran for i in sql_siswa])
+        print(f"Data == {data}")
+
+        print(f"Sql siswa {sql_siswa}")
+        for i in sql_siswa:
+            print(f"{i.siswa.first_name} {i.siswa.last_name}")
+
+        response = make_response(
+            render_template(
+                "admin/letter_report/result_rekap_bulan.html",
+                sql_siswa=sql_siswa,
+                data=data,
+                sql_ket=sql_ket,
+                AbsensiModel=AbsensiModel,
+                func=func,
+            )
+        )
+        return response
 
     response = make_response(
         render_template(
@@ -81,5 +137,4 @@ def rekap_absen():
             sql_wali_=sql_wali_(),
         )
     )
-
     return response
