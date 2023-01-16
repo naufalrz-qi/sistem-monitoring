690e00abed89ec85ecfc00ab1a9f1aa39599ad3e
Ari Efendi <ariefendi992@gmail.com>
2023-01-16 22:34:08 +0800

    modified g_bk controller, add sidebar b.bk

--- app/site/controller/guru_bk_controller.py
+++ app/site/controller/guru_bk_controller.py
@@ -7,13 +7,26 @@ from flask import (
     Blueprint,
     make_response,
 )
+from flask_login import login_required, current_user
+from app.models.master_model import GuruBKModel
 
 guru_bk = Blueprint(
-    "guru_bk", __name__, static_folder="../static/", template_folder="../templates/"
+    "guru_bk",
+    __name__,
+    static_folder="../static/",
+    template_folder="../templates/",
+    url_prefix="/guru-bk/",
 )
 
 
+def get_guru_bk():
+    sql = GuruBKModel.query.filter_by(guru_id=current_user.id).first()
+
+
 @guru_bk.route("index")
+@login_required
 def index():
-    response = make_response(render_template("guru_bk/index_bk.html"))
+    response = make_response(
+        render_template("guru_bk/index_bk.html", guru_bk=get_guru_bk())
+    )
     return response
